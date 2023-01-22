55808a1b46cd9dd9113aee5d2da5312be1f4daae
Suyash Garg <sgarg@FH-LT-SGAR-02.cpex.com>
2023-01-22 20:21:12 +0000

    do something for a landing page

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,9 @@
-import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
-}
+import '../styles/main.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <Component {...pageProps} />
+);
+
+export default MyApp;
