36f9e357793818f46fbd52f1f685e9fd78572a05
Param Singh <me@param.codes>
2023-01-27 15:58:02 +0530

    Add script to improve transcriptions

--- transcribe/db/schema_changes/20230127_add_improvement_column.py
+++ transcribe/db/schema_changes/20230127_add_improvement_column.py
@@ -3,6 +3,7 @@ from transcribe.db import init_db
 
 def main():
     """Add a column to the transcription table to store improvement data."""
+    print("Adding column!")
     connection = init_db()
     cursor = connection.cursor()
     cursor.execute(
@@ -12,3 +13,8 @@ def main():
     )
     connection.commit()
     connection.close()
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
--- transcribe/db/transcription.py
+++ transcribe/db/transcription.py
@@ -57,6 +57,41 @@ def get_one_unfinished_transcription(db) -> Union[dict, None]:
     return None
 
 
+def get_one_unimproved_transcription(db) -> Union[dict, None]:
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT uuid, link, result
+          FROM transcription
+         WHERE improvement is NULL
+           AND result is NOT NULL
+      ORDER BY created ASC
+         LIMIT 1;
+    """
+    )
+    result = cursor.fetchone()
+
+    if result:
+        return {
+            "uuid": result[0],
+            "link": result[1],
+            "result": result[2],
+        }
+    return None
+
+
+def add_improvement(db, improved_text: str, uuid: str) -> None:
+    """Add improved text to the improvement column for transcription with given UUID"""
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        UPDATE transcription SET improvement = ? WHERE uuid = ?;
+    """,
+        (improved_text, uuid),
+    )
+    db.commit()
+
+
 def populate_transcription(db, uuid: str, result: str) -> None:
     """Add result to a transcription"""
     cursor = db.cursor()
