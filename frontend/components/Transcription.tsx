7638edc7b5932978afada1e1883f43a3afc88de6
Param Singh <me@param.codes>
2023-01-28 22:39:25 +0530

    more margin stuff

--- frontend/components/Transcription.tsx
+++ frontend/components/Transcription.tsx
@@ -3,7 +3,7 @@ import { Heading } from "@chakra-ui/react";
 export const Transcription = ({ text }: { text: string }) => {
   return (
     <div style={{ whiteSpace: "pre-line" }}>
-      <Heading as={"h1"} size="3xl">
+      <Heading as={"h1"} size="3xl" style={{marginBottom: "10px"}}>
         Transcription
       </Heading>
       <p>{text}</p>
